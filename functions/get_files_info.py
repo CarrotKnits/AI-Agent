8c146c2b9d6a6ee1ac4867b29ea878c4a09ea90c
CarrotKnits <carrotsandcarrots@gmail.com>
2025-06-16 18:25:14 -0500

    ADD check, if input is directory, if directory input in working directory, start of returning a string representing directory contents

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,12 +1,29 @@
 import os
 
-def get_files_info(working_directory, directory=None):
-    if directory not in working_directory:
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if os.path.isdir(directory) == False:
-        return f'Error: "{directory}" is not a directory'
+def get_files_info(working_directory, directory=None):    
+    # Get absolute path for the directory input or, if no directory input, get absolute path for the working directory
+    if directory == None:
+        absolute_path = os.path.abspath(working_directory)
     else:
-        pass
+        absolute_path = os.path.abspath(directory)
+    
+    # Check if the directory input is in the working directory
+    if absolute_path.startswith(os.path.abspath(working_directory)):
+        # Check if the directory input is indeed a directory or not
+        if os.path.isdir(directory) == False:
+            return f'Error: "{directory}" is not a directory'
+        # Return file/subdirectory info for all files/subdirectories contained in the directory input
+        contents_list = []
+        directory_contents = os.listdir(directory)
+        for content in directory_contents:
+            contents_list.append(f"- {content}: file_size={os.path.getsize(os.path.join(absolute_path, content))}, is_dir={os.path.isdir(os.path.join(absolute_path, content))}")
+        return "\n".join(contents_list)
+    # Return error string if directory input not in working directory
+    else:
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+        
+
 
     # Format for file info to be returned
     # - README.md: file_size=1032 bytes, is_dir=False
