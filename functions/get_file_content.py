bbdc4930606519670d6ba6c930384b5adcbfeb70
CarrotKnits <carrotsandcarrots@gmail.com>
2025-06-18 15:31:23 -0500

    CREATE functions 'get_file_content' and 'cat_file_content'

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,2 +1,13 @@
-def get_file_content(working_directory, file_path):
+import os
+
+def cat_file_content(target_file_path):
     pass
+
+def get_file_content(working_directory, file_path):
+        target_file_path = os.path.abspath(file_path)
+        # Check if the target file is in the working directory
+        if target_file_path.startswith(os.path.abspath(working_directory)):
+            return cat_file_content(target_file_path)
+        # Return error string if target file not in working directory
+        else:
+            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
