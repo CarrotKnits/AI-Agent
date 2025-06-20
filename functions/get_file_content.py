f7ff594af2a506b5057a9d736605db62c948f768
CarrotKnits <carrotsandcarrots@gmail.com>
2025-06-20 14:51:10 -0500

    FINISH get_file_content function, FIX try block logic error for correct appending of truncation message

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,26 +1,27 @@
 import os
 
-def file_contents(directory, file_path):
-    # Maxixum characters to be returned from file
-    MAX_CHARS = 10000
-    # Return contents of target file as a string
-    try:
-        with open(os.path.join(directory, file_path), "r") as f:
-            file_content_string = f.read(MAX_CHARS) + f'[...File "{file_path}" truncated at 10000 characters]'
-    except OSError as e:
-         return f"Error: {e}"
-    return file_content_string
-     
-
 def get_file_content(working_directory, file_path):
-    # If file == None return an error
-    if file_path == None:
-        return f"Error: No file selected"
+    target_file_path = os.path.abspath(os.path.join(working_directory,file_path))
+
+    # Check if the target file is in working directory
+    if target_file_path.startswith(os.path.abspath(working_directory)):
+        # Check if file is really a file or not
+        if os.path.isfile(target_file_path) == False:
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+        # Maxixum characters to be returned from file
+        MAX_CHARS = 10000
+    
+        # Return contents of target file as a string
+        try:
+            with open(target_file_path, "r") as f:
+                file_content_string = f.read()
+                # Trencate file to 10000 chars if count is > 10000
+                if len(file_content_string) > 10000:
+                    file_content_string = file_content_string[:MAX_CHARS] + f'[...File "{file_path}" truncated at 10000 characters]'
+        except OSError as e:
+            return f"Error: {e}"
+        return file_content_string
+    # Return error string if target file not in working directory
     else:
-        target_file_path = os.path.abspath(file_path)
-        # Check if the target file is in working directory
-        if target_file_path.startswith(os.path.abspath(working_directory)):
-            return file_contents(target_file_path)
-        # Return error string if target file not in working directory
-        else:
-            f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+       return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
