5033e7a1befe706dbe80c5d1ff912eb7f4fa3807
CarrotKnits <carrotsandcarrots@gmail.com>
2025-06-21 13:24:58 -0500

    FIX logic errors in 'write_file.py'

--- functions/write_file.py
+++ functions/write_file.py
@@ -8,14 +8,10 @@ def write_file(working_directory, file_path, content):
 # Check if the target file is in working directory
     if target_file_path.startswith(os.path.abspath(working_directory)):
         # Check if file_path exists
-        if os.path.exists(target_file_path) == False:
-            # Check if file is really a file or not
-            if os.path.isfile(target_file_path) == False:
-                return f'Error: File not found or is not a regular file: "{file_path}"'
-            
+        if os.path.exists(target_file_path):
             # Overwrite contents of file
             try:
-                with open(file_path, "w") as f:
+                with open(target_file_path, "w") as f:
                     f.write(content)
                     return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
             except OSError as e:
@@ -24,13 +20,13 @@ def write_file(working_directory, file_path, content):
         else:
             # Create file path
             try:
-                os.makedirs(target_file_path)
+                os.makedirs(os.path.dirname(target_file_path), exist_ok=True)
             except OSError as e:
                 return f'Error: {e}'
             
             # Write contents to newly created file
             try:
-                with open(file_path, "w") as f:
+                with open(target_file_path, "w") as f:
                     f.write(content)
                     return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
             except OSError as e:
@@ -38,4 +34,4 @@ def write_file(working_directory, file_path, content):
 
     # Return error string if target file not in working directory
     else:
-        f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
